345a69a4523d57ed2fec8c06c2ee767840ef9c44
katerina <mel.katya@mail.ru>
2025-09-19 16:18:55 +0300

    combining upper and lower case letters

--- fastapi_application/crypto/cesar.py
+++ fastapi_application/crypto/cesar.py
@@ -1,19 +1,17 @@
-def encoding(text: str, shift: int = 5) -> str:
+def encrypt(text: str, shift: int = 5) -> str:
     encoded_text = []
     for sym in text:
-        if "A" <= sym <= "Z":
-            encoded_text.append(chr((ord(sym) + shift - ord("A")) % 26 + ord("A")))
-        elif "a" <= sym <= "z":
-            encoded_text.append(chr((ord(sym) + shift - ord("a")) % 26 + ord("a")))
-        elif "А" <= sym <= "Я":
-            encoded_text.append(chr((ord(sym) + shift - ord("А")) % 32 + ord("А")))
-        elif "а" <= sym <= "я":
-            encoded_text.append(chr((ord(sym) + shift - ord("а")) % 32 + ord("а")))
+        if ("A" <= sym <= "Z") or ("a" <= sym <= "z"):
+            new_char = chr((ord(sym.upper()) + shift - ord("A")) % 26 + ord("A"))
+            encoded_text.append(new_char if sym.isupper() else new_char.lower())
+        elif ("А" <= sym <= "Я") or ("а" <= sym <= "я"):
+            new_char = chr((ord(sym.upper()) + shift - ord("А")) % 32 + ord("А"))
+            encoded_text.append(new_char if sym.isupper() else new_char.lower())
         else:
             encoded_text.append(sym)
 
     return "".join(encoded_text)
 
 
-def decoding(encoded_text: str, shift: int = 5) -> str:
-    return encoding(encoded_text, -shift)
+def decrypt(encoded_text: str, shift: int = 5) -> str:
+    return encrypt(encoded_text, -shift)
