eee021f87f0163fa64591298d6d50af1cce8d536
katerina <mel.katya@mail.ru>
2025-09-19 16:23:13 +0300

    add prefix and tags

--- fastapi_application/main.py
+++ fastapi_application/main.py
@@ -6,7 +6,7 @@ from api import router as api_router
 
 
 app = FastAPI()
-app.include_router(api_router)
+app.include_router(api_router, prefix="/crypto", tags=["Crypto"])
 
 
 if __name__ == "__main__":
