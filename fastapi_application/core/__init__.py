cb110060ccc148ec90d6bf69e076e537ad52593b
katerina <mel.katya@mail.ru>
2025-09-20 17:51:50 +0300

    add Alphabet detail

---
+++ fastapi_application/core/__init__.py
@@ -0,0 +1,14 @@
+__all__ = (
+    "eng_idx",
+    "eng_letters",
+    "rus_idx",
+    "rus_letters",
+)
+
+from core.config import settings
+
+eng_idx = settings.alphabet.eng_idx
+eng_letters = settings.alphabet.eng_letters
+
+rus_idx = settings.alphabet.rus_idx
+rus_letters = settings.alphabet.rus_letters
