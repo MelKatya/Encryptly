0d6d29a4667f719e3388f9de42868f7764e6bc1f
katerina <mel.katya@mail.ru>
2025-09-20 17:52:21 +0300

    update cesar with alphabet

--- fastapi_application/crypto/cesar.py
+++ fastapi_application/crypto/cesar.py
@@ -1,12 +1,17 @@
+from core import eng_letters, eng_idx, rus_letters, rus_idx
+
+
 def encrypt(text: str, shift: int = 5) -> str:
     encoded_text = []
     for sym in text:
-        if ("A" <= sym <= "Z") or ("a" <= sym <= "z"):
-            new_char = chr((ord(sym.upper()) + shift - ord("A")) % 26 + ord("A"))
+        if sym.upper() in eng_letters:
+            new_char = eng_idx[(eng_letters.get(sym.upper()) + shift) % len(eng_idx)]
             encoded_text.append(new_char if sym.isupper() else new_char.lower())
-        elif ("А" <= sym <= "Я") or ("а" <= sym <= "я"):
-            new_char = chr((ord(sym.upper()) + shift - ord("А")) % 32 + ord("А"))
+
+        elif sym.upper() in rus_letters:
+            new_char = rus_idx[(rus_letters.get(sym.upper()) + shift) % len(rus_idx)]
             encoded_text.append(new_char if sym.isupper() else new_char.lower())
+
         else:
             encoded_text.append(sym)
 
@@ -14,4 +19,4 @@ def encrypt(text: str, shift: int = 5) -> str:
 
 
 def decrypt(encoded_text: str, shift: int = 5) -> str:
-    return encrypt(encoded_text, -shift)
+    return encrypt(text=encoded_text, shift=-shift)
