03e6e701049e5bfc21ae60464bd77dcd93b24941
katerina <mel.katya@mail.ru>
2025-09-21 18:48:18 +0300

    add atbash crypt

---
+++ fastapi_application/crypto/atbash.py
@@ -0,0 +1,18 @@
+from core import eng_letters, eng_idx, rus_letters, rus_idx
+
+
+def crypt(text: str) -> str:
+    encoded_text = []
+    for sym in text:
+        if sym.upper() in eng_letters:
+            new_char = eng_idx[len(eng_idx) - eng_letters[sym.upper()] - 1]
+            encoded_text.append(new_char if sym.isupper() else new_char.lower())
+        elif sym.upper() in rus_letters:
+            new_char = rus_idx[len(rus_idx) - rus_letters[sym.upper()] - 1]
+            encoded_text.append(new_char if sym.isupper() else new_char.lower())
+        else:
+            encoded_text.append(sym)
+
+    return "".join(encoded_text)
+
+
