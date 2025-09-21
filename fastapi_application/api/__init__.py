17266cc8df213a9718ec2f4ec444e48bbeef828f
katerina <mel.katya@mail.ru>
2025-09-21 19:42:30 +0300

    create router atbash crypt

--- fastapi_application/api/__init__.py
+++ fastapi_application/api/__init__.py
@@ -2,8 +2,10 @@ from fastapi import APIRouter
 
 from .cesar import router as cesar_router
 from .vigenere import router as vigenere_router
+from .atbash import router as atbash_router
 
 router = APIRouter()
 
 router.include_router(cesar_router, prefix="/cesar")
 router.include_router(vigenere_router, prefix="/vigenere")
+router.include_router(atbash_router, prefix="/atbash")
