308fb159058a978119691f73bf9f74c77c4f616d
katerina <mel.katya@mail.ru>
2025-09-24 21:43:45 +0300

    add encrypt endpoint

--- fastapi_application/api/rsa.py
+++ fastapi_application/api/rsa.py
@@ -11,3 +11,8 @@ def generate_keys(data: RsaGenerateKey) -> RsaKeyRead:
     private_key, public_key = create_keys(key_size=data.key_size)
     return RsaKeyRead(private_key=private_key, public_key=public_key)
 
+
+@router.post("/encrypt")
+def rsa_encrypt(data: RsaEncrypt) -> str:
+    encrypted_text = encrypt(text=data.text, public_pem=data.public_key)
+    return encrypted_text
